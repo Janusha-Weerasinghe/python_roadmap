31dad56735f9fc505f76d12b506f5c3529d7fc55
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-05-23 00:38:42 +0530

    input() and Exercise 3

---
+++ Madlibs_game.py
@@ -0,0 +1,16 @@
+#Matlibs game
+# word game where you create a game
+# by filtering in blanks with random words
+adjective1 = input("Enter an adjective(description): ")
+noun1 = input("enter the noune(person,place , things): ")
+adjective2 =input ("Enter an adjective discription: ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter the adjective (description): ")
+
+
+
+
+print (f"Today I want to a {adjective1} zoo")
+print (f"In an exibit, I saw a {noun1}")
+print (f"{noun1} was {adjective2} and {verb1}")
+print (f"I was {adjective3}!")
