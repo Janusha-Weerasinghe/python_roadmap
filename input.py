a2af11a3fd15649f30873c6e4e9ffed9a87af3f0
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-05-22 23:30:30 +0530

    input() and Exercise 1

---
+++ input.py
@@ -0,0 +1,19 @@
+#input() = A function that prompts the user to enter data
+# return the entered data as a string
+
+name = input("What is your name ? : ")
+age = input ("how old are you? : ")
+print(f"Hello {name}!")
+print(f"You are {age} years old")
+
+#Exercise 1 Rectangle Area calc
+length = float(input("enter the length : "))
+width = float(input ("Enter the width :"))
+area = length * width
+
+print (f"The area is : {area}cm²")
+
+#without the float  or int we cannot divide or do any calculation
+# Windows : NumLock+Alt+0178
+#Mac : Control + Command + Space
+
