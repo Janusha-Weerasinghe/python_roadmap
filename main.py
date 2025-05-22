ce9270d8fcf0683cb49db612e0b6a74dd5b5756d
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-05-22 12:45:02 +0530

    variables

--- main.py
+++ main.py
@@ -2,3 +2,34 @@
 
 print ("I like pizza")
 print ("I like cake")
+
+#variables = A container for a value (String, integer , float, boolean)
+#            A variable behaves as if it was the value it contains
+
+#Strings
+first_name = "Bro"
+email = "j@123.com"
+print(first_name)
+print(f"Hello {first_name}")
+print(f"My mail is : {email}")
+
+#Intergers
+age = 25
+quantity= 3
+print(f"you are {age} is old")
+print (f"you are buying {quantity} items")
+
+#Float
+price = 10.99
+gpa =3.7
+print (f"price is LKR{price}")
+print (f"My gpa is {gpa}")
+
+#Boolean
+is_student = False
+print (f"Are you student? : {is_student }")
+
+if is_student:
+    print ("You are student")
+else:
+    print("You are not student")
