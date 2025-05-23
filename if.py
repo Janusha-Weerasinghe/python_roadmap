2788be46143868739bad8f4eaf6690aee1373b4d
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-05-23 14:03:39 +0530

    exercise and theory

---
+++ if.py
@@ -0,0 +1,30 @@
+#if = Do some code only if some condition is true
+#Else do somthing else
+
+# age = int(input("Enter your age: "))
+#
+# if age >=18:
+#     print("You are now signed up!")
+# elif age <0 :
+#     print("you are not born yet")
+# else :
+#     print("You must be 18+ to sign up")
+###################################################
+# response = input ("would you like to food? (Y/N):")
+#
+# if response =="Y":
+#     print("you have food!")
+# elif response =="":
+#     print("you didn't type anything")
+# else :
+#     print("No food you have!")
+
+#################################################
+#boolean
+
+for_sale =False
+
+if for_sale:
+    print("this item for sale")
+else:
+    print("This item is not for sale")
