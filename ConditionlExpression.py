16e2e1d9337bfd4f7b03c410248b667be42d813b
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-05-23 18:40:04 +0530

    exercise and theory

---
+++ ConditionlExpression.py
@@ -0,0 +1,19 @@
+#conditional Expression = A oneline shortcut for the if-else statement (ternary operator)
+            #             Print or assign one of two values based on a condition
+#                         x if condition else Y
+
+num =25
+a= 6
+b= 7
+
+
+#print ("positive" if num > 0 else "Negative")
+
+#result = "Even" if num%2==0 else "Odd"
+#print (result)
+
+max_num= a if a>b else b
+min_num = a if a<b else b
+
+
+print (min_num)
